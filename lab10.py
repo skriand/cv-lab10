4b2e188cc1977425c1e076b0c7a38899dbdbf4a6
Andrey <andrey.skri2011@gmail.com>
2022-12-29 17:13:40 +0300

    Add files via upload

--- lab10.py
+++ lab10.py
@@ -38,32 +38,27 @@ db = pd.read_csv('data1.csv', delimiter=',')
 db['embedding'] = db['embedding'].apply(lambda x: np.frombuffer(base64.b64decode(bytes(x[2:-1], encoding='ascii')), dtype=np.int32))
 
 
-def main():
-    st.title('Search for similar images')
-    uploaded_file = st.file_uploader("Choose an image...", type=["png", "jpg", "jpeg", "webp", "tiff"])
-    if uploaded_file is None:
-        pass
+st.title('Search for similar images')
+uploaded_file = st.file_uploader("Choose an image...", type=["png", "jpg", "jpeg", "webp", "tiff"])
+if uploaded_file is None:
+    pass
+else:
+    image = Image.open(uploaded_file).convert("RGB")
+    image = ImageOps.exif_transpose(image)
+    st.image(image)
+    img_opencv = np.array(image)
+    img_opencv = img_opencv[:, :, ::-1].copy()
+    if  math.isnan(sum(vectorize(img_opencv))):
+        st.success("Bad image")
+        pass 
     else:
-        image = Image.open(uploaded_file).convert("RGB")
-        image = ImageOps.exif_transpose(image)
-        st.image(image)
-        img_opencv = np.array(image)
-        img_opencv = img_opencv[:, :, ::-1].copy()
-        if  math.isnan(sum(vectorize(img_opencv))):
-            st.success("Bad image")
-            pass 
-        else:
-            pathes = get_neighbours_pathes(db, get_k_neighbours(vectorize(img_opencv), db, 3))
-            st.success("Found similar images")
-            col = st.columns(3)
-            for i in range(len(pathes)):
-                try:
-                    with col[i]:
-                        similar_image = Image.open(pathes[i])
-                        st.image(similar_image, width=200)
-                except UnidentifiedImageError:
-                    pass
-
-
-if __name__ == '__main__':
-    main()
+        pathes = get_neighbours_pathes(db, get_k_neighbours(vectorize(img_opencv), db, 3))
+        st.success("Found similar images")
+        col = st.columns(3)
+        for i in range(len(pathes)):
+            try:
+                with col[i]:
+                    similar_image = Image.open(pathes[i])
+                    st.image(similar_image, width=200)
+            except UnidentifiedImageError:
+                pass
